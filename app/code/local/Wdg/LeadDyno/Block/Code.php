6b29e4706aaaf1bf05f7d1258b22b7c482388cf6
Vazgen Ovakimyan <vazgen.ovakimyan@wdg.com.ua>
2013-11-20 17:49:15 +0200

    AutoWatcher

--- app/code/local/Wdg/LeadDyno/Block/Code.php
+++ app/code/local/Wdg/LeadDyno/Block/Code.php
@@ -14,4 +14,12 @@ class Wdg_LeadDyno_Block_Code extends Mage_Core_Block_Template {
 		return Mage::getStoreConfig(Wdg_LeadDyno_Helper_Data::XML_PATH_PUBLIC_KEY);
 	}
 	
+	public function isWatcherActive(){
+		return Mage::getStoreConfigFlag(Wdg_LeadDyno_Helper_Data::XML_PATH_WATCHER_ACTIVE);
+	}
+	
+	public function getWatcherSelector(){
+		return Mage::getStoreConfig(Wdg_LeadDyno_Helper_Data::XML_PATH_WATCHER_SELECTOR);
+	}
+	
 }
--- app/code/local/Wdg/LeadDyno/Helper/Data.php
+++ app/code/local/Wdg/LeadDyno/Helper/Data.php
@@ -6,4 +6,7 @@ class Wdg_LeadDyno_Helper_Data extends Mage_Core_Helper_Abstract {
 	const XML_PATH_PUBLIC_KEY = 'leaddyno/general/keypublic';
 	const XML_PATH_PRIVATE_KEY = 'leaddyno/general/keyprivate';
 	
+	const XML_PATH_WATCHER_ACTIVE = 'leaddyno/watcher/active';
+	const XML_PATH_WATCHER_SELECTOR = 'leaddyno/watcher/selector';
+	
 }
